e195e8c4b14c4de606ffac3e691abeed09cb75af
Nate Notermann <nate.notermann@gmail.com>
2022-07-23 15:54:20 -0500

    created onclick function, goes to new page at /details

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import Details from '../Details/Details'
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
           <MovieList />
         </Route>
         
+        <Route path="/details" exact>
+          <Details />
+        </Route>
         {/* Details page */}
 
         {/* Add Movie page */}
@@ -21,3 +25,17 @@ function App() {
 
 
 export default App;
+
+// READ ME
+// Nice CSS
+  // cards and grids css
+
+  // notes
+
+
+// movie poster onclick to /details view for that movie
+// Details should show ALL GENRES for that movie
+  // title, description, image. Use Saga and redux for this 
+
+  // Hint : You can make a GET request for a specific movie. Remember req.params and :id?
+
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,16 @@
+
+function Details () {
+
+    return(
+        <>
+        <h2> Details Page</h2>
+        <h3>Title here</h3>
+        <h4> Genres Here</h4>
+        <p>
+            Movie info here
+        </p>
+        </>
+    )
+}
+
+export default Details;
