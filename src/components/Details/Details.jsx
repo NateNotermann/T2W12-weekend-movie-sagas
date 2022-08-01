1a7e560f705968a1ee1a40c877ef4f5c53e71141
Nate Notermann <nate.notermann@gmail.com>
2022-08-01 01:00:57 -0500

    SQL code doesn't array_agg correctly. Genres dont align perfectly. Avatars 3rd genre is push into a new row. tthroughs entire app oof when trying to get any movie genres

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,69 +1,74 @@
 import '../Details/Details';
-import {useHistory } from 'react-router-dom';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import ERD from '../ERD/ERD';
 import { useState, useEffect } from 'react';
 
 
-function Details (  ) {
+function Details() {
 
     const history = useHistory();
     const currentMovie = useSelector(store => store.currentMovie)
     const genres = useSelector(store => store.genres)
     const dispatch = useDispatch();
-    
-    
-// ------ Buttons ------ //
-    function backTolist () {
+
+
+    // ------ Buttons ------ //
+    function backTolist() {
         history.push('/')
     }
 
-    function toErd (){
+    function toErd() {
         history.push('/erd')
     }
 
-    function currentMovieFunction (){ // checks currentMovie
-            console.log('currentMovie.movie (Details.jsx)', currentMovie.movie);
-            console.log('GENRE.id (Details.jsx)', genres.id );
-            // console.log works, so state is updating..
+    function currentMovieFunction() { // checks currentMovie
+        console.log('currentMovie.movie (Details.jsx)', currentMovie.movie);
+        console.log('GENRE.id (Details.jsx)', genres.id);
+        // console.log works, so state is updating..
     }
 
 
 
     useEffect(() => {
         // dispatch({ type: 'FETCH_MOVIES' });
-         dispatch({ type: 'GET_GENRES_DB' }); 
-    }, [] );
+        dispatch({ type: 'GET_GENRES_DB' });
+    }, []);
 
 
-    return(
+    return (
         <>
-        <header>        
-            <h2> Details Page</h2>            
-        </header>
-        
-        <h3>{currentMovie.movie.title}</h3>
-        <p> {currentMovie.movie.description}</p>
-        <img src={currentMovie.movie.poster}></img>
-        <h3>{genres.array_agg}</h3>
-        <h4>Genres</h4>
-        {genres.map((genres, i ) =>  {
-            return(
-                
-                <h4 key={i}>{genres.array_agg}</h4>
-            )
-        })}
+            <header>
+                <h2> Details Page</h2>
+            </header>
 
-        <div>        
-        <button onClick={backTolist}> back to list</button>
-        <button onClick={toErd}> Go to ERD</button>
-        <button onClick={currentMovieFunction}>currentMovie</button>
-        </div>
+            <h3><span>{currentMovie.movie.title}'s movie number is: {currentMovie.movie.id}</span></h3>
+            <p> {currentMovie.movie.description}</p>
+            <img src={currentMovie.movie.poster}></img>
 
-        <Route path="/erd" exact>
-          <ERD />
-        </Route>
+            <h3>{genres.array_agg}</h3>
+            <h4>It's Genres are:</h4>
+            {genres.map((genres, i) => {
+                console.log(i);
+                if (i === currentMovie.movie.id -1 ) {
+                    console.log(' i === currentMovie.id!!!');
+                    return (<>
+                        <h1>{genres.array_agg}</h1>
+                        </>
+                    )
+                }
+            })}
+
+            <div>
+                <button onClick={backTolist}> back to list</button>
+                <button onClick={toErd}> Go to ERD</button>
+                <button onClick={currentMovieFunction}>currentMovie</button>
+            </div>
+
+            <Route path="/erd" exact>
+                <ERD />
+            </Route>
         </>
     )
 }
